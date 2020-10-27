19b73ea274c5acc756f902342074a121b00ba0a0
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-10-27 23:54:32 +0330

    Install testbench

---
+++ src/Facades/Contents.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Hsy\Html\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class Contents extends Facade
+{
+
+}
---
+++ src/HsyContentsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Hsy\Contents;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class HsyContentsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
